2940e22b8eb4b0db6f30881a4929d437fc26addd
osbson <orebenson@hotmail.com>
2024-12-03 10:46:54 +0000

    Edited file names£

---
+++ src/pages/PointsStatement.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+import '../fonts.css';
+
+const PointsStatement = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <div>
+                
+
+            </div>
+        </ThemeProvider>
+    );
+}
+
+export default PointsStatement
