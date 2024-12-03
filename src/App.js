4289176f2ad28bc36f66489e83ff86f1e2f6bb7b
AnaCamarasan <ana.camarasan@gmail.com>
2024-12-03 09:57:34 +0000

    changed files names

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button';
 import theme from './theme';
 import './fonts.css';
 import NavBar from './components/NavBar';
-import Home from './pages/home';
+import Home from './pages/Home';
 
 
 function App() {
