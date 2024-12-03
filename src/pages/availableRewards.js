277fdc44b68f2134ccec923b81b28afdeadeaef8
AnaCamarasan <ana.camarasan@gmail.com>
2024-12-03 10:40:02 +0000

    added routes for all pages

--- src/pages/availableRewards.js
+++ src/pages/availableRewards.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme';
 import '../fonts.css';
-import NavBar from '../components/NavBar';
 
 const AvailableRewards = () => {
     return (
         <ThemeProvider theme={theme}>
             <div>
-                <NavBar />
 
             </div>
         </ThemeProvider>
