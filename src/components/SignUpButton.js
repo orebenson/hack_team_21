a1589e08dc8db16b58c34dae52138479c8947460
osbson <orebenson@hotmail.com>
2024-12-03 14:39:11 +0000

    Conflicts

--- src/components/SignUpButton.js
+++ src/components/SignUpButton.js
@@ -4,8 +4,15 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button'; // Corrected import for Button
 import Box from '@mui/material/Box'; // Import for Box
 import theme from '../theme';
+import { useNavigate } from 'react-router-dom';
 
 const SignUpButton = () => {
+
+  const navigate = useNavigate(); // Initialize the navigate function
+
+  const handleClaimClick = () => {
+    navigate('/'); // Navigate to the desired route
+  };
   return (
     <ThemeProvider theme={theme}>
       <Box
@@ -18,7 +25,7 @@ const SignUpButton = () => {
           mt: 4,
         }}
       >
-        <Button variant ="contained" size="large" color="primary" sx={{ mx: 'auto', width: 320, height: 60, p:1 }}>
+        <Button onClick={handleClaimClick} variant ="contained" size="large" color="primary" sx={{ mx: 'auto', width: 320, height: 60, p:1 }}>
           <Typography variant="body1" component="div">
               Sign up now 
           </Typography>
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import TextCard from '../components/textCard'; // Adjust the import path as necessary
-import SignUpButton from '../components/SignUpButton'; 
+import SignUpButton from '../components/SignUpButton';
 import theme from '../theme';
 import logo from '../assets/stable_rewards_logo.png';
 import Box from '@mui/material/Box';
@@ -9,47 +9,49 @@ import Typography from '@mui/material/Typography';
 
 const signup = () => {
 
-    const benefits = [
-        'Claim vouchers using points earned and save money on your shopping',
-        'Each month you save the larger the amount points you get that month',
-        'Multiplier of points increases each month you save consecutively'
 
-      ];
 
-      const requirements = [
-        'Have a monthly savings account open',
-        'Deposit £50 to £250 each month to ensure the highest possible award',
-      ];
 
-    return (
-      <ThemeProvider theme={theme}>
-        <div>
-            <Typography variant="h6" component="div" sx={{ fontWeight: 'bold', textAlign: 'center', mt: 2 }}>
-            Join Stable Rewards today!
-            </Typography>
-            <Box
-            sx={{
+  const benefits = [
+    'Claim vouchers using points earned and save money on your shopping',
+    'Each month you save the larger the amount points you get that month',
+    'Multiplier of points increases each month you save consecutively'
+
+  ];
+
+  const requirements = [
+    'Have a monthly savings account open',
+    'Deposit £50 to £250 each month to ensure the highest possible award',
+  ];
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <Typography variant="h6" component="div" sx={{ fontWeight: 'bold', textAlign: 'center', mt: 2 }}>
+          Join Stable Rewards today!
+        </Typography>
+        <Box
+          sx={{
             display: 'flex',
             justifyContent: 'center', // Only horizontal alignment
             p: 2, // Optional padding around the content
-            }}
-            >
-            <img src={logo} alt="Logo" style={{ height: '200px' }} />
-            </Box>
-            <TextCard sx ={{p: 2}}
-                heading="Benefits"
-                body={benefits}
-            />
-            <TextCard sx ={{p: 2}}
-                heading="Requirements"
-                body={requirements}
-            />
+          }}
+        >
+          <img src={logo} alt="Logo" style={{ height: '200px' }} />
+        </Box>
+        <TextCard sx={{ p: 2 }}
+          heading="Benefits"
+          body={benefits}
+        />
+        <TextCard sx={{ p: 2 }}
+          heading="Requirements"
+          body={requirements}
+        />
 
-            <SignUpButton/>
-        </div>
-      </ThemeProvider>
-    );
-  };
-  
-  export default signup;
-  
+        <SignUpButton />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default signup;
