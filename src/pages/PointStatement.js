4c646487428399ddcb72ad1557d2cad0fe16d873
osbson <orebenson@hotmail.com>
2024-12-03 10:40:36 +0000

    Added pointstatement page

---
+++ src/pages/PointStatement.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+import '../fonts.css';
+
+const PointStatement = () => {
+    return (
+        <ThemeProvider theme={theme}>
+            <div>
+                
+
+            </div>
+        </ThemeProvider>
+    );
+}
+
+export default PointStatement
