39790b1ce63eff7a95ee22d6ffe838a24044fa98
Stylianos Kyzis <stylianos.kz@hotmail.com>
2024-12-03 10:38:57 +0000

    Card

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import DonutChart from '../components/DonutChart';
+import RewardsCard from '../components/RewardsCard'; // Adjust the import path as necessary
 import theme from '../theme';
 
 const Home = () => {
   const saved = 250;
   const target = 250;
   const month = 'December';
+  const multiplier = 1.2;
+  const pointsEarned = 3000;
+  const totalPoints = 4500;
 
   return (
     <ThemeProvider theme={theme}>
@@ -16,9 +20,14 @@ const Home = () => {
         alignItems: 'center', 
         minHeight: '100vh', 
         padding: '20px',
-        justifyContent: 'flex-start'
+        justifyContent: 'flex-start',
       }}>
         <DonutChart percentage={(saved / target) * 100} saved={saved} target={target} month={month} />
+        <RewardsCard 
+          multiplier={multiplier}
+          pointsEarned={pointsEarned}
+          totalPoints={totalPoints}
+        />
       </div>
     </ThemeProvider>
   );
